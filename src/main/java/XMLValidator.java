205e8df548bcfab943475b9751d99f1470520a58
TomashevskiiVladimir <vladimir.tomashevskii@tekschool.us>
2023-06-23 13:10:47 -0400

    17

--- src/main/java/XMLValidator.java
+++ src/main/java/XMLValidator.java
@@ -1,9 +1,6 @@
 
 import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
@@ -15,21 +12,15 @@ public class XMLValidator {
 
     public static boolean validateXMLAgainstXSD(String xmlFilePath, String xsdFilePath) {
         try {
-            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = schemaFactory.newSchema(new File(xsdFilePath));
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdFilePath));
             Validator validator = schema.newValidator();
-            JAXBContext jaxbContext = JAXBContext.newInstance(EmployeeManagementType.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            unmarshaller.setSchema(schema);
-
-            EmployeeManagementType employeeManagement = (EmployeeManagementType) unmarshaller.unmarshal(new File(xmlFilePath));
-            validator.validate((Source) unmarshaller.getUnmarshallerHandler().getResult());
-
-            return true;
-        } catch (JAXBException | SAXException | IOException e) {
+            validator.validate(new StreamSource(new File(xmlFilePath)));
+        } catch ( SAXException | IOException e) {
             e.printStackTrace();
             return false;
         }
+        return true;
     }
 
     public static void main(String[] args) {
